900a0763b9d1ab4498cf5c1feedf3693186e14f9
Ebrahim-Elkbbany <ana.elkbany@gmail.com>
2023-08-01 17:17:10 +0200

    refactor init state & add home view & navigate to home view

--- lib/Features/Splash/presentation/views/widgets/splash_view_body.dart
+++ lib/Features/Splash/presentation/views/widgets/splash_view_body.dart
@@ -1,6 +1,9 @@
 import 'package:bookly/Features/Splash/presentation/views/widgets/animated_text.dart';
+import 'package:bookly/Features/home/presentaion/views/home_view.dart';
+import 'package:bookly/constants.dart';
 import 'package:bookly/core/utils/assets.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class SplashViewBody extends StatefulWidget {
   const SplashViewBody({Key? key}) : super(key: key);
@@ -14,21 +17,11 @@ class _SplashViewBodyState extends State<SplashViewBody>
   late AnimationController animationController;
   late Animation<Offset> slidingAnimation;
 
-
-
   @override
   void initState() {
     super.initState();
-    animationController = AnimationController(
-      vsync: this,
-      duration: const Duration(seconds: 1),
-    );
-
-    slidingAnimation =
-        Tween<Offset>(begin: const Offset(0,2), end: Offset.zero)
-            .animate(animationController);
-
-    animationController.forward();
+    initAnimationSplashView();
+    navigateToHome();
   }
 
   @override
@@ -49,6 +42,28 @@ class _SplashViewBodyState extends State<SplashViewBody>
         ),
         AnimatedText(slidingAnimation: slidingAnimation),
       ],
+    );
+  }
+
+  void initAnimationSplashView() {
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+    slidingAnimation =
+        Tween<Offset>(begin: const Offset(0, 2), end: Offset.zero)
+            .animate(animationController);
+
+    animationController.forward();
+  }
+
+  void navigateToHome() {
+    Future.delayed(
+      const Duration(seconds: 2),
+      () {
+        Get.to(const HomeView(),
+            transition: Transition.fade, duration: kTransitionDuration);
+      },
     );
   }
 }
