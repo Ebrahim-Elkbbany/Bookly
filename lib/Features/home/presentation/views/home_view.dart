8485e74099bb7806bbd5a0fae80c58a8df4aca79
Ebrahim-Elkbbany <ana.elkbany@gmail.com>
2023-08-04 05:10:24 +0200

    finish app styles

--- lib/Features/home/presentation/views/home_view.dart
+++ lib/Features/home/presentation/views/home_view.dart
@@ -17,7 +17,7 @@ class HomeView extends StatelessWidget {
           SizedBox(height: 50,),
           Padding(
             padding: EdgeInsets.only(left: 24),
-            child: Text('Best Seller',style: Styles.fontStyle18),
+            child: Text('Best Seller',style: Styles.textStyle18),
           ),
         ],
       ),
